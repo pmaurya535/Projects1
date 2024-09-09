6ab6b248e1d5788361b7a550d0ae8fed1551766c
pmaurya535 <pradipkumarmaurya12345@gmail.com>
2024-09-09 17:39:12 +0530

    my first commit

---
+++ app/src/main/java/com/example/loginregistration/Login.java
@@ -0,0 +1,25 @@
+package com.example.loginregistration;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        final EditText usernameET = findViewById(R.id.usernameET);
+        final EditText passwordET = findViewById(R.id.passwordET);
+        final EditText signUpBtn = findViewById(R.id.signInBtn);
+
+    }
+}
